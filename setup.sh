f1f83924f360ef32e9d2349cecace1b75646eb75
Ben Melton <ben@themeltons.net>
2013-03-13 16:57:26 -0400

    add email folder to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh i3"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim zshrc oh-my-zsh i3 Xdefaults email"    # list of files/folders to symlink in homedir
 
 ##########
 
